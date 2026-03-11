669ab3071946f2813581976f6f46b830a05dc14b
marius-kengne <haroldkengne@gmail.com>
2026-03-11 11:58:04 +0100

    init project

---
+++ src/main/java/fr/harxen/secure_java_devsecops/SecureJavaDevsecopsApplication.java
@@ -0,0 +1,13 @@
+package fr.harxen.secure_java_devsecops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureJavaDevsecopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureJavaDevsecopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/harxen/secure_java_devsecops/SecureJavaDevsecopsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.harxen.secure_java_devsecops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureJavaDevsecopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
